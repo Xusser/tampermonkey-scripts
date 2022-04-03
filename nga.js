450ad127823eb438bcb4ff880ea15c5500a4e811
Xusser <xus97@live.com>
2022-04-03 22:57:15 +0800

    Update nga.js

--- nga.js
+++ nga.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NGA网址重定向
 // @namespace    https://github.com/Xusser/tampermonkey-scripts/blob/master/nga.js
-// @version      0.5
+// @version      0.6
 // @description  重定向NGA所有域名至 nga.178.com
 // @updateURL    https://raw.githubusercontent.com/Xusser/tampermonkey-scripts/master/nga.js
 // @downloadURL  https://raw.githubusercontent.com/Xusser/tampermonkey-scripts/master/nga.js
